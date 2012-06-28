38dabc51039ef8ad71f2e84e2e939a8983726df0
Artyom Olshevskiy <siasiamail@gmail.com>
2012-06-28 04:06:41 +0300

    Stabilize working dir

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' fonts.conf.tpl > fonts.conf
 cd $(dirname $0)/..
+perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' fonts.conf.tpl > fonts.conf
 LANG=C FONTCONFIG_FILE=$(readlink -f fonts.conf) phantomjs/bin/phantomjs js/hello.js
